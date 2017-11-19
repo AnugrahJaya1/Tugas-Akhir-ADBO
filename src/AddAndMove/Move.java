f46b55b2bf3106840e9b9bcf6ea7982457d7abc5
samuelPangihutan <32352864+samuelPangihutan@users.noreply.github.com>
2017-11-19 13:34:44 +0700

    new Update

--- src/AddAndMove/Move.java
+++ src/AddAndMove/Move.java
@@ -14,10 +14,11 @@ import com.jme3.scene.Node;
 public interface Move {
     /**
      * Method ini berfungsi untuk menggerakan sebuah model
+     * @param speed mempengaruhi keceptan untuk menggerakan model
      * @param tpf tpf didapatkan dari Method Update yang berasal dari AbstractAppState
      * @param localRootNode Node yang memiliki child Node
      * @param player node yang memiliki Models yang akan di Load
      */
     
-    public void move(float tpf,Node localRootNode,Node player);
+    public void move(float speed,float tpf,Node localRootNode,Node player);
 }
